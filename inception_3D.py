d029a69eff0cd4525ce9d00bb2c043bb4afd6a4b
zhang1995123 <49333939+zhang1995123@users.noreply.github.com>
2021-06-28 23:46:04 +0800

    Add files via upload

--- inception_3D.py
+++ inception_3D.py
@@ -1,15 +1,26 @@
 from __future__ import division
 
+from collections import namedtuple
 import warnings
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.jit.annotations import Optional
+from torch import Tensor
 
 
+__all__ = ['Inception3', 'inception_3', 'InceptionOutputs']
 
-__all__ = ['Inception3', 'inception_3']
+model_urls = {
+    # Inception v3 ported from TensorFlow
+    'inception_v3_google': 'https://download.pytorch.org/models/inception_v3_google-1a9a5a14.pth',
+}
+
+InceptionOutputs = namedtuple('InceptionOutputs', ['logits', 'aux_logits'])
+InceptionOutputs.__annotations__ = {'logits': torch.Tensor, 'aux_logits': Optional[torch.Tensor]}
 
 # Script annotations failed with _GoogleNetOutputs = namedtuple ...
+
 
 def inception_3(pretrained=False, progress=True, **kwargs):
     r"""Inception v3 model architecture from
@@ -30,62 +41,57 @@ def inception_3(pretrained=False, progress=True, **kwargs):
             was trained on ImageNet. Default: *False*
     """
 
+    if 'transform_input' not in kwargs:
+        kwargs['transform_input'] = True
+    if 'aux_logits' in kwargs:
+        original_aux_logits = kwargs['aux_logits']
+        kwargs['aux_logits'] = True
+    else:
+        original_aux_logits = True
     model = Inception3(**kwargs)
+    if not original_aux_logits:
+        model.aux_logits = False
+        del model.AuxLogits
     
     return model
 
 
 class Inception3(nn.Module):
 
-    def __init__(self, num_classes=10,
+    def __init__(self, num_classes=1000, aux_logits=True, transform_input=False,
                  inception_blocks=None):
         super(Inception3, self).__init__()
         
         conv_block = BasicConv3d
-        
         inception_a = InceptionA
         inception_b = InceptionB
         inception_c = InceptionC
         inception_d = InceptionD
         inception_e = InceptionE
-        #inception_aux = InceptionAux
+        inception_aux = InceptionAux
 
-        # self.Conv3d_1a_3x3 = conv_block(3, 32, kernel_size=3, stride=1, padding=(0, 1, 1))
-
-        self.Conv3d_2a_3x3 = conv_block(3, 16, kernel_size=3)
-        self.Conv3d_2b_3x3 = conv_block(16, 32, kernel_size=3, padding=1)
-
-        self.Conv3d_3a_3x3 = conv_block(32, 64, kernel_size=3, padding=1)
-        self.Conv3d_3b_1x1 = conv_block(64, 128, kernel_size=1)
-        self.Conv3d_4a_3x3 = conv_block(128, 192, kernel_size=3)
-
+        self.aux_logits = aux_logits
+        self.transform_input = transform_input
+        self.Conv3d_1a_3x3 = conv_block(3, 32, kernel_size=3, stride=1, padding=(0, 1, 1))
+        self.Conv3d_2a_3x3 = conv_block(32, 32, kernel_size=3)
+        self.Conv3d_2b_3x3 = conv_block(32, 64, kernel_size=3, padding=1)
+        self.Conv3d_3b_1x1 = conv_block(64, 80, kernel_size=1)
+        self.Conv3d_4a_3x3 = conv_block(80, 192, kernel_size=3)
         self.Mixed_5b = inception_a(192, pool_features=32)
         self.Mixed_5c = inception_a(256, pool_features=64)
         self.Mixed_5d = inception_a(288, pool_features=64)
         self.Mixed_6a = inception_b(288)
-        self.Mixed_6b = inception_c(384, channels_7x7=64)
-        self.Mixed_6c = inception_c(384, channels_7x7=80)
-        self.Mixed_6d = inception_c(384, channels_7x7=80)
-        self.Mixed_6e = inception_c(384, channels_7x7=96)
-        self.Mixed_7a = inception_d(384)
-        self.Mixed_7b = inception_e(640)
-        self.Mixed_7c = inception_e(1024)
-        self.fc = nn.Linear(1024, num_classes)
+        self.Mixed_6b = inception_c(768, channels_7x7=128)
+        self.Mixed_6c = inception_c(768, channels_7x7=160)
+        self.Mixed_6d = inception_c(768, channels_7x7=160)
+        self.Mixed_6e = inception_c(768, channels_7x7=192)
+        if aux_logits:
+            self.AuxLogits = inception_aux(768, num_classes)
+        self.Mixed_7a = inception_d(768)
+        self.Mixed_7b = inception_e(1280)
+        self.Mixed_7c = inception_e(2048)
+        self.fc = nn.Linear(2048, num_classes)
 
-        self.init_weight()
-
-    def init_weight(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Conv3d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm3d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def init_weight_(self):
         for m in self.modules():
             if isinstance(m, nn.Conv3d) or isinstance(m, nn.Linear):
                 import scipy.stats as stats
@@ -99,60 +105,89 @@ class Inception3(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-    def forward(self, x):
+    def _transform_input(self, x):
+        if self.transform_input:
+            x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
+            x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
+            x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+            x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
+        return x
+
+    def _forward(self, x):
         # N x 3 x 299 x 299 for 2D
         # N x 3 x 43 x 149 x 149 
-        # x = self.Conv3d_1a_3x3(x)
-        # N x 3 x 43 x 81 x 81
+        x = self.Conv3d_1a_3x3(x)
+        # N x 32 x 41 x 149 x 149
         x = self.Conv3d_2a_3x3(x)
-        # N x 16 x 41 x 79 x 79
+        # N x 32 x 39 x 147 x 147
         x = self.Conv3d_2b_3x3(x)
-        # N x 32 x 41 x 79 x 79
+        # N x 64 x 39 x 147 x 147
         x = F.max_pool3d(x, kernel_size=3, stride=(1, 2, 2))
-
-        # N x 32 x 39 x 39 x 39
-        x = self.Conv3d_3a_3x3(x)
-        # N x 64 x 39 x 39 x 39  scale=1/2
+        # N x 64 x 37 x 73 x 73
         x = self.Conv3d_3b_1x1(x)
-        # N x 128 x 39 x 39 x 39
+        # N x 80 x 37 x 73 x 73
         x = self.Conv3d_4a_3x3(x)
-        # N x 192 x 37 x 37 x 37
-        x = F.max_pool3d(x, kernel_size=3, stride=2, padding=1)
+        # N x 192 x 35 x 71 x 71
+        x = F.max_pool3d(x, kernel_size=3, stride=(1, 2, 2), padding=(1, 0, 0))
 
-        # N x 192 x 19 x 19 x 19 下同
+        # N x 192 x 35 x 35 x 35 下同
         x = self.Mixed_5b(x)
-        # N x 256 x 19 x 19 x 19
+        # N x 256 x 35 x 35 x 35
         x = self.Mixed_5c(x)
-        # N x 288 x 19 x 19 x 19
+        # N x 288 x 35 x 35 x 35
         x = self.Mixed_5d(x)
-        # N x 288 x 19 x 19 x 19
+        # N x 288 x 35 x 35 x 35
         x = self.Mixed_6a(x)
-        # N x 384 x 9 x 9 x 9
+        # N x 768 x 17 x 17 x 17
         x = self.Mixed_6b(x)
-        # N x 384 x 9 x 9 x 9
+        # N x 768 x 17 x 17 x 17
         x = self.Mixed_6c(x)
-        # N x 384 x 9 x 9 x 9
+        # N x 768 x 17 x 17 x 17
         x = self.Mixed_6d(x)
-        # N x 384 x 9 x 9 x 9
+        # N x 768 x 17 x 17 x 17
         x = self.Mixed_6e(x)
-        # N x 384 x 9 x 9 x 9
+        
+        # N x 768 x 17 x 17 x 17
+        aux_defined = self.training and self.aux_logits
+        if aux_defined:
+            aux = self.AuxLogits(x)
+        else:
+            aux = None
+
+        # N x 768 x 17 x 17 x 17
         x = self.Mixed_7a(x)
-        # N x 640 x 4 x 4 x 4
+        # N x 1280 x 8 x 8 x 8
         x = self.Mixed_7b(x)
-        # N x 1024 x 4 x 4 x 4
+        # N x 2048 x 8 x 8 x 8
         x = self.Mixed_7c(x)
-        # N x 1024 x 4 x 4 x 4
+        # N x 2048 x 8 x 8 x 8
         # Adaptive average pooling
         x = F.adaptive_avg_pool3d(x, (1, 1, 1))
-        # N x 1024 x 1 x 1 x 1
+        # N x 2048 x 1 x 1 x 1
         x = F.dropout(x, training=self.training)
-        # N x 1024 x 1 x 1 x 1
+        # N x 2048 x 1 x 1 x 1
         x = torch.flatten(x, 1)
-        # N x 1024
+        # N x 2048
         x = self.fc(x)
         # N x 1000 (num_classes)
+        return x, aux
 
-        return x
+    def eager_outputs(self, x, aux):
+        # type: (Tensor, Optional[Tensor]) -> InceptionOutputs
+        if self.training and self.aux_logits:
+            return InceptionOutputs(x, aux)
+        else:
+            return x
+
+    def forward(self, x):
+        x = self._transform_input(x)
+        x, aux = self._forward(x)
+        aux_defined = self.training and self.aux_logits
+        if not aux_defined:
+            warnings.warn("Scripted Inception3 always returns Inception3 Tuple")
+            return InceptionOutputs(x, aux)
+        else:
+            return self.eager_outputs(x, aux)
 
 
 class InceptionA(nn.Module):
@@ -199,13 +234,11 @@ class InceptionB(nn.Module):
         super(InceptionB, self).__init__()
         if conv_block is None:
             conv_block = BasicConv3d
-        self.branch3x3 = conv_block(in_channels, 192, kernel_size=3, stride=2)
+        self.branch3x3 = conv_block(in_channels, 384, kernel_size=3, stride=2)
 
-        self.branch3x3dbl_1 = conv_block(in_channels, 32, kernel_size=1)
-        self.branch3x3dbl_2 = conv_block(32, 48, kernel_size=3, padding=1)
-        self.branch3x3dbl_3 = conv_block(48, 48, kernel_size=3, stride=2)
-
-        self.branch_conv = conv_block(in_channels, 144, kernel_size=3, padding=1)
+        self.branch3x3dbl_1 = conv_block(in_channels, 64, kernel_size=1)
+        self.branch3x3dbl_2 = conv_block(64, 96, kernel_size=3, padding=1)
+        self.branch3x3dbl_3 = conv_block(96, 96, kernel_size=3, stride=2)
 
     def _forward(self, x):
         branch3x3 = self.branch3x3(x)
@@ -214,8 +247,7 @@ class InceptionB(nn.Module):
         branch3x3dbl = self.branch3x3dbl_2(branch3x3dbl)
         branch3x3dbl = self.branch3x3dbl_3(branch3x3dbl)
 
-        branch_pool = self.branch_conv(x)
-        branch_pool = F.max_pool3d(branch_pool, kernel_size=3, stride=2)
+        branch_pool = F.max_pool3d(x, kernel_size=3, stride=2)
 
         outputs = [branch3x3, branch3x3dbl, branch_pool]
         return outputs
@@ -231,23 +263,20 @@ class InceptionC(nn.Module):
         super(InceptionC, self).__init__()
         if conv_block is None:
             conv_block = BasicConv3d
-        self.branch1x1 = conv_block(in_channels, 96, kernel_size=1)
+        self.branch1x1 = conv_block(in_channels, 192, kernel_size=1)
 
         c7 = channels_7x7
         self.branch7x7_1 = conv_block(in_channels, c7, kernel_size=1)
         self.branch7x7_2 = conv_block(c7, c7, kernel_size=(1, 1, 7), padding=(0, 0, 3))
         self.branch7x7_3 = conv_block(c7, c7, kernel_size=(1, 7, 1), padding=(0, 3, 0))
-        self.branch7x7_4 = conv_block(c7, 96, kernel_size=(7, 1, 1), padding=(3, 0, 0))
+        self.branch7x7_4 = conv_block(c7, 192, kernel_size=(7, 1, 1), padding=(3, 0, 0))
 
         self.branch7x7dbl_1 = conv_block(in_channels, c7, kernel_size=1)
-        self.branch7x7dbl_2 = conv_block(c7, c7, kernel_size=(1, 1, 7), padding=(0, 0, 3))
+        self.branch7x7dbl_2 = conv_block(c7, c7, kernel_size=(7, 1, 1), padding=(3, 0, 0))
         self.branch7x7dbl_3 = conv_block(c7, c7, kernel_size=(1, 7, 1), padding=(0, 3, 0))
-        self.branch7x7dbl_4 = conv_block(c7, c7, kernel_size=(7, 1, 1), padding=(3, 0, 0))
-        self.branch7x7dbl_5 = conv_block(c7, c7, kernel_size=(1, 1, 7), padding=(0, 0, 3))
-        self.branch7x7dbl_6 = conv_block(c7, c7, kernel_size=(1, 7, 1), padding=(0, 3, 0))
-        self.branch7x7dbl_7 = conv_block(c7, 96, kernel_size=(7, 1, 1), padding=(3, 0, 0))
+        self.branch7x7dbl_4 = conv_block(c7, 192, kernel_size=(1, 1, 7), padding=(0, 0, 3))
 
-        self.branch_pool = conv_block(in_channels, 96, kernel_size=1)
+        self.branch_pool = conv_block(in_channels, 192, kernel_size=1)
 
     def _forward(self, x):
         branch1x1 = self.branch1x1(x)
@@ -261,9 +290,7 @@ class InceptionC(nn.Module):
         branch7x7dbl = self.branch7x7dbl_2(branch7x7dbl)
         branch7x7dbl = self.branch7x7dbl_3(branch7x7dbl)
         branch7x7dbl = self.branch7x7dbl_4(branch7x7dbl)
-        branch7x7dbl = self.branch7x7dbl_5(branch7x7dbl)
-        branch7x7dbl = self.branch7x7dbl_6(branch7x7dbl)
-        branch7x7dbl = self.branch7x7dbl_7(branch7x7dbl)
+        # branch7x7dbl = self.branch7x7dbl_5(branch7x7dbl)
 
         branch_pool = F.avg_pool3d(x, kernel_size=3, stride=1, padding=1)
         branch_pool = self.branch_pool(branch_pool)
@@ -282,14 +309,14 @@ class InceptionD(nn.Module):
         super(InceptionD, self).__init__()
         if conv_block is None:
             conv_block = BasicConv3d
-        self.branch3x3_1 = conv_block(in_channels, 96, kernel_size=1)
-        self.branch3x3_2 = conv_block(96, 160, kernel_size=3, stride=2)
+        self.branch3x3_1 = conv_block(in_channels, 192, kernel_size=1)
+        self.branch3x3_2 = conv_block(192, 320, kernel_size=3, stride=2)
 
-        self.branch7x7x3_1 = conv_block(in_channels, 96, kernel_size=1)
-        self.branch7x7x3_2 = conv_block(96, 96, kernel_size=(1, 1, 7), padding=(0, 0, 3))
-        self.branch7x7x3_3 = conv_block(96, 96, kernel_size=(1, 7, 1), padding=(0, 3, 0))
-        self.branch7x7x3_4 = conv_block(96, 96, kernel_size=(7, 1, 1), padding=(3, 0, 0))
-        self.branch7x7x3_5 = conv_block(96, 96, kernel_size=3, stride=2)
+        self.branch7x7x3_1 = conv_block(in_channels, 192, kernel_size=1)
+        self.branch7x7x3_2 = conv_block(192, 192, kernel_size=(1, 1, 7), padding=(0, 0, 3))
+        self.branch7x7x3_3 = conv_block(192, 192, kernel_size=(1, 7, 1), padding=(0, 3, 0))
+        self.branch7x7x3_4 = conv_block(192, 192, kernel_size=(7, 1, 1), padding=(3, 0, 0))
+        self.branch7x7x3_5 = conv_block(192, 192, kernel_size=3, stride=2)
 
     def _forward(self, x):
         branch3x3 = self.branch3x3_1(x)
@@ -316,20 +343,20 @@ class InceptionE(nn.Module):
         super(InceptionE, self).__init__()
         if conv_block is None:
             conv_block = BasicConv3d
-        self.branch1x1 = conv_block(in_channels, 160, kernel_size=1)
+        self.branch1x1 = conv_block(in_channels, 320, kernel_size=1)
 
-        self.branch3x3_1 = conv_block(in_channels, 128, kernel_size=1)
-        self.branch3x3_2a = conv_block(128, 128, kernel_size=(1, 1, 3), padding=(0, 0, 1))
-        self.branch3x3_2b = conv_block(128, 128, kernel_size=(1, 3, 1), padding=(0, 1, 0))
-        self.branch3x3_2c = conv_block(128, 128, kernel_size=(3, 1, 1), padding=(1, 0, 0))
+        self.branch3x3_1 = conv_block(in_channels, 256, kernel_size=1)
+        self.branch3x3_2a = conv_block(256, 256, kernel_size=(1, 1, 3), padding=(0, 0, 1))
+        self.branch3x3_2b = conv_block(256, 256, kernel_size=(1, 3, 1), padding=(0, 1, 0))
+        self.branch3x3_2c = conv_block(256, 256, kernel_size=(3, 1, 1), padding=(1, 0, 0))
 
-        self.branch3x3dbl_1 = conv_block(in_channels, 224, kernel_size=1)
-        self.branch3x3dbl_2 = conv_block(224, 128, kernel_size=3, padding=1)
-        self.branch3x3dbl_3a = conv_block(128, 128, kernel_size=(1, 1, 3), padding=(0, 0, 1))
-        self.branch3x3dbl_3b = conv_block(128, 128, kernel_size=(1, 3, 1), padding=(0, 1, 0))
-        self.branch3x3dbl_3c = conv_block(128, 128, kernel_size=(3, 1, 1), padding=(1, 0, 0))
+        self.branch3x3dbl_1 = conv_block(in_channels, 448, kernel_size=1)
+        self.branch3x3dbl_2 = conv_block(448, 256, kernel_size=3, padding=1)
+        self.branch3x3dbl_3a = conv_block(256, 256, kernel_size=(1, 1, 3), padding=(0, 0, 1))
+        self.branch3x3dbl_3b = conv_block(256, 256, kernel_size=(1, 3, 1), padding=(0, 1, 0))
+        self.branch3x3dbl_3c = conv_block(256, 256, kernel_size=(3, 1, 1), padding=(1, 0, 0))
 
-        self.branch_pool = conv_block(in_channels, 96, kernel_size=1)
+        self.branch_pool = conv_block(in_channels, 192, kernel_size=1)
 
     def _forward(self, x):
         branch1x1 = self.branch1x1(x)
@@ -410,7 +437,8 @@ if __name__=='__main__':
     model = model.cuda()
     # print(model)
 
-    in_ = torch.randn(10, 3, 43, 81, 81)
-    in_ = in_.cuda()
-    out_ = model(in_)
-    print(out_.shape)
+    input_ = torch.randn(3, 3, 43, 149, 149)
+    input_ = input_.cuda()
+    (out, aux) = model(input_)
+    print(aux.shape)
+    print(out.shape)
